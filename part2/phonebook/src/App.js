6cb55a25ee2dc9b5525839e4b7e20a64cc4a77f0
jmgcito <joshuamguillen@gmail.com>
2023-02-22 14:20:45 -0800

    Finished Part2c&d Exercises 2.11-2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import personsServices from "./services/persons";
 
 const Filter = ({ nameFilter, onChange }) => {
   return (
@@ -34,44 +35,39 @@ const PersonForm = ({
   );
 };
 
-const Person = ({ person }) => {
+const Person = ({ person, onClick }) => {
   return (
     <div key={person.name}>
-      {person.name} {person.number}{" "}
+      {person.name} {person.number} <button onClick={onClick}>delete</button>
     </div>
   );
 };
 
-const Persons = ({ personsToShow }) => {
-  console.log(personsToShow);
+const Persons = ({ personsToShow, handleDeletePerson }) => {
   return (
     <div>
       {personsToShow.map((person) => (
-        <Person key={person.name} person={person} />
+        <Person
+          key={person.name}
+          person={person}
+          onClick={() => handleDeletePerson(person.id)}
+        />
       ))}
     </div>
   );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [nameFilter, setNameFilter] = useState("");
 
-  const isUniquePerson = (newPerson, persons) => {
-    for (let person of persons) {
-      if (person.name === newPerson.name) {
-        return false;
-      }
-    }
-    return true;
-  };
+  useEffect(() => {
+    personsServices
+      .getAll()
+      .then((initialPersons) => setPersons(initialPersons));
+  }, []);
 
   const handleNewName = (e) => {
     setNewName(e.target.value);
@@ -89,11 +85,32 @@ const App = () => {
       number: newNumber,
     };
 
-    if (isUniquePerson(newPerson, persons)) {
-      const newPersons = persons.concat(newPerson);
-      setPersons(newPersons);
+    if (
+      persons.find((person) => newPerson.name === person.name) === undefined
+    ) {
+      personsServices
+        .create(newPerson)
+        .then((returnedPerson) => setPersons(persons.concat(returnedPerson)));
     } else {
-      window.alert(`${newName} is already added to the phonebook`);
+      if (
+        window.confirm(
+          `${newName} is already added to the phonebook, replace the old number with a new one?`
+        )
+      ) {
+        console.log("Confirmed");
+
+        const id = persons.find((person) => newPerson.name === person.name).id;
+
+        personsServices
+          .update(id, newPerson)
+          .then((returnedPerson) =>
+            setPersons(
+              persons.map((person) =>
+                person.name !== newPerson.name ? person : returnedPerson
+              )
+            )
+          );
+      }
     }
     setNewName("");
     setNewNumber("");
@@ -101,6 +118,17 @@ const App = () => {
 
   const handleNewFilter = (e) => {
     setNameFilter(e.target.value);
+  };
+
+  const handleDeletePerson = (id) => {
+    const personToDelete = persons.find((person) => person.id === id);
+    if (window.confirm(`Delete ${personToDelete.name}`)) {
+      personsServices
+        .deleteObject(id)
+        .then((response) =>
+          setPersons(persons.filter((person) => person.id !== id))
+        );
+    }
   };
 
   const personsToShow =
@@ -125,7 +153,10 @@ const App = () => {
       />
 
       <h2>Numbers</h2>
-      <Persons personsToShow={personsToShow} />
+      <Persons
+        personsToShow={personsToShow}
+        handleDeletePerson={handleDeletePerson}
+      />
     </div>
   );
 };
